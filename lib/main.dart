5a816f121032179d01c84acd0438fcd1a453c289
Artur Peres Meneses <147016690+peres1710@users.noreply.github.com>
2024-05-22 11:09:26 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,147 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor: Colors.black,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      'AC', 80, 80, 24,const Color.fromARGB(255, 195, 192, 192),Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '+/-', 80, 80, 24,const Color.fromARGB(255, 195, 192, 192),Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '%', 80, 80, 40,const Color.fromARGB(255, 195, 192, 192),Colors.black),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom('÷', 80, 80, 40, Colors.orange,Colors.white),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '7', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '8', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '9', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom('×', 80, 80, 40, Colors.orange,Colors.white),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '4', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '5', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '6', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom('-', 80, 80, 55, Colors.orange,Colors.white),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '1', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '2', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '3', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom('+', 80, 80, 40, Colors.orange,Colors.white),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      '0', 80, 170, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom(
+                      ',', 80, 80, 40, const Color.fromARGB(255, 64, 63, 63),Colors.white),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: buildButtom('=', 80, 80, 40, Colors.orange,Colors.white),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox buildButtom(String txt, double altura, double largura,
+      double fontSize, Color backgroundColor, Color textColor) {
+    return SizedBox(
+      height: altura,
+      width: largura,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+        ),
+        onPressed: () {},
+        child: Text(
+          txt,
+          style: TextStyle(
+              color: textColor, fontSize: fontSize),
         ),
       ),
     );
